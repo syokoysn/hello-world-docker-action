0569ca1bf2bf4d16e3d58e030fc2681fb4de6808
syokoysn <syoko_ysn@outlook.jp>
2021-04-21 22:25:44 +0900

    check

--- .github/actions/action2/test.py
+++ .github/actions/action2/test.py
@@ -4,4 +4,5 @@ if __name__ == '__main__':
     print("Github Actions test")
     check = os.environ.get('SECRET_KEY_TEST')
     print("check => ",check)
+    print(check == "SECRET_KEY_TEST")
     print('done')
