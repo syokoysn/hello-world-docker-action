d9f6695d328f59c9574e89e5b6134aa819ff3eff
syokoysn <syoko_ysn@outlook.jp>
2021-05-01 01:41:21 +0000

    apdate

--- .github/actions/action3/check.py
+++ .github/actions/action3/check.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     for i in os.listdir("./"):
         print(os.path.abspath(i))
     print(__file__)
+    sys.path.append("/github/workspace")
     print(sys.path)
     import mypackage
     print(mypackage.hello.hello())
