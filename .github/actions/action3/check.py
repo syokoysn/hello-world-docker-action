e120268d73ec100591d47e929c14b0d1a4207e97
syokoysn <syoko_ysn@outlook.jp>
2021-05-01 00:43:00 +0000

    update

--- .github/actions/action3/check.py
+++ .github/actions/action3/check.py
@@ -4,5 +4,7 @@ import os
 
 if __name__ == '__main__':
     print(os.listdir("./"))
+    for i in os.listdir("./"):
+        print(os.path.abspath(i))
     print(__file__)
     #print(mypackage.hello.hello())
